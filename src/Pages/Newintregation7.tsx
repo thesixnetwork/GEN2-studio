8bac00228193142fd5d899d815fea33addf3341d
suppawich <suppawich@six.network>
2023-09-13 17:19:23 +0700

    edit newintregation7

--- src/Pages/Newintregation7.tsx
+++ src/Pages/Newintregation7.tsx
@@ -1,77 +1,217 @@
-import React from 'react'
-import { Button } from '@mui/material'
-import DeleteIcon from '@mui/icons-material/Delete';
-import logo1 from '../pic/trash-bin_5028066 1.png'
-import logo2 from "../pic/Vector 4.png"
-import logo3 from '../pic/Vector 7.png'
-import Delete from "../pic/Group 27.png"
-import Add from '../pic/Group 40.png'
+import React from "react";
+import { Button } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import logo1 from "../pic/trash-bin_5028066 1.png";
+import logo2 from "../pic/Vector 4.png";
+import logo3 from "../pic/Vector 7.png";
+import Delete from "../pic/Group 27.png";
+import Add from "../pic/Group 40.png";
 
-import Conectwalet from '../component/Connectwallet'
-import Stepper2 from '../component/Stepper2';
-import Darkbg from '../component/Alert/Darkbg';
-import AlertCard from './Alert/AlertCard';
-import NextPageButton from '../component/NextPageButton';
+import Conectwalet from "../component/Connectwallet";
+import Stepper2 from "../component/Stepper2";
+import Darkbg from "../component/Alert/Darkbg";
+import AlertCard from "./Alert/AlertCard";
+import NextPageButton from "../component/NextPageButton";
+
+import { useState } from 'react'
 
 const NewIntregation7 = () => {
-    const [isShow, setIsShow] = React.useState(false);
-    return (
-        <div className='w-full flex justify-center ' >
-            <div className='w-full h-full fixed  flex justify-center items-center bg-gradient-24  from-white to-[#7A8ED7]'>
-                <div className='w-[1280px] h-[832px] bg-gradient-24 to-gray-700 from-gray-300 rounded-2xl flex justify-between p-4 shadow-lg shadow-black/20 dark:shadow-black/40'>
-                    <div className='w-full h-full px-[20px]'>
-                        <div>
-                            <Stepper2 ActiveStep={3}></Stepper2>
-                            <div className='w-[931px] h-[1px] bg-[#D9D9D9]'></div>
-                        </div>
-                        <div className='mt-7 ml-10 w-full  grid-cols-3 grid justify-center items-center '>
-                            <div className='w-[227px] h-[157px] bg-transparent border border-white rounded-xl p-3 '>
-                                <div className='w-[24px] h-[24px]  flex justify-center items-center rounded-full absolute ml-[185px] mt-[-7px]'>
-                                    <img src={Delete}></img>
-                                </div>
-                                <div className='flex text-[14px] mt-2'>Name :&ensp;<p className='underline'>background</p></div>
-                                <div className='flex text-[14px]'>Data type :&ensp;<p className='underline'>string</p></div>
-                                <div className='flex text-[14px]'>Display value field :&ensp;<p className='underline'>value</p></div>
-                                <div className='flex text-[14px]'>Display option :&ensp;<p className='underline'>opensea</p></div>
-                                <div className='flex text-[14px]'>Trait type :&ensp;<p className='underline'>Background</p></div>
-                            </div>
-                            <div className='w-[227px] h-[157px] flex justify-center items-center bg-transparent border border-white rounded-xl p-3 hover:scale-105 cursor-pointer duration-300 '>
-                                <img src={Add}></img>
+  const [isShow, setIsShow] = useState(false);
 
-                            </div>     
-                        </div>
-                        <NextPageButton TextTitle='Next' BorderRadius={0} NextPage='/newintregation/8' FontSize={40}  ></NextPageButton>
-                    </div>
-                    <div className='w-2/6 h-5/6 flex flex-col items-end  '>
-                        <Conectwalet></Conectwalet>
-                        <div className='w-[266px] h-[414px] border-[1px] border-white rounded-xl mt-[30px] flex flex-col items-center py-[10px] z-10' >
-                            <p className='text-[24px] w-[231px] font-bold text-white text-center'>Origin Token Attributes</p>
-                            <p className='text-[16px] w-[216px]  text-white pt-[15px]'>&emsp;Mostly NFT token will need attributes of it called metadata. Origin attributes is the static type of metadata. And it cannot be change ( but it can be override by token attributes we will discuss next step ).</p>
-                            <p className='text-[16px] w-[216px]  text-white pt-[15px]'>&emsp; You can edit value of attributes by double click the value and change to your proper design.</p>
-                        </div>
-                        <div onClick={() => { setIsShow(!isShow) }} className='absolute text-[50px] mt-[750px] ml-[1150px] cursor-pointer hover:scale-150 hover:text-[#262f50] duration-500'>?</div>
-                    </div>
-                </div>
-                {isShow ?
-                    <div className='absolute duration-500' onClick={() => { setIsShow(!isShow) }}>
-                        <Darkbg ></Darkbg>
-                        <div className='absolute mt-[140px] ml-[-350px] z-20'>
-                            <img src={logo3} className='mt-[80px] mr-[150px] rotate-[160deg] '></img>
-                            <p className='ml-[300px] mt-[-60px] absolute w-full font-bold'>Double Click to edit</p>
-                        </div>
-                        <div className='absolute mt-[0px] ml-[-430px] z-20'>
-                            <img src={logo2} className='mt-[50px] mr-[200px] rotate-[160deg] '></img>
-                            <p className='ml-[-190px] mt-[-185px] absolute w-full font-bold'>Click to delete component</p>
-                        </div>
+  const [editing, setEditing] = useState({
+    cardIndex: null,
+    field: null,
+  });
+  const [text, setText] = useState([
+    {
+      name: "firtname",
+      dataType: "data1",
+      traitType: "trait1",
+    },
+    {
+      name: "secondname",
+      dataType: "data2",
+      traitType: "trait2",
+    },
+  ]);
 
-                    </div>
-                    :
-                    <div></div>
+  const handleClick = (cardIndex, field) => {
+    setEditing({ cardIndex, field });
+  };
 
-                }
+  const handleChange = (e, field, index) => {
+    const updatedText = [...text];
+    updatedText[index][field] = e.target.value;
+    setText(updatedText);
+  };
+
+  const handleBlur = () => {
+    setEditing({ cardIndex: null, field: null });
+  };
+
+  const handleCreateAttribute = () => {
+    const newAttribute = {
+      name: null,
+      dataType: null,
+      traitType: null,
+    };
+
+    setText([...text, newAttribute]);
+  };
+  return (
+    <div className="w-full flex justify-center ">
+      <div className="w-full h-full fixed  flex justify-center items-center bg-gradient-24  from-white to-[#7A8ED7]">
+        <div className="w-[1280px] h-[832px] bg-gradient-24 to-gray-700 from-gray-300 rounded-2xl flex justify-between p-4 shadow-lg shadow-black/20 dark:shadow-black/40">
+          <div className="w-full h-full px-[20px]">
+            <div>
+              <Stepper2 ActiveStep={3}></Stepper2>
+              <div className="w-[931px] h-[1px] bg-[#D9D9D9]"></div>
             </div>
+            <div className="mt-7 ml-10 w-full h-5/6 grid-cols-3 grid gap-y-1 overflow-scroll">
+              {text.map((item, index) => (
+                <div
+                  key={index}
+                  className="w-[227px] h-[157px] bg-transparent border-solid border border-white-600 rounded-xl p-3 "
+                >
+                  <div className="w-[24px] h-[24px] bg-red-500 flex justify-center items-center rounded-full ml-[185px] mt-[-7px]">
+                    <DeleteIcon className="scale-75"></DeleteIcon>
+                  </div>
+                  <>
+                    <div
+                      onClick={() => handleClick(index, "name")}
+                      className="flex text-[14px] mt-2"
+                    >
+                      Name :&ensp;{" "}
+                      {editing.cardIndex === index &&
+                      editing.field === "name" ? (
+                        <input
+                          type="text"
+                          value={item.name}
+                          onChange={(e) => handleChange(e, "name", index)}
+                          onBlur={handleBlur}
+                          className="flex text-[14px] bg-transparent h-[21px] w-20 outline-none border-solid"
+                        />
+                      ) : (
+                        <p className="underline">{item.name}</p>
+                      )}
+                    </div>
+                    <div
+                      onClick={() => handleClick(index, "dataType")}
+                      className="flex text-[14px]"
+                    >
+                      Data type :&ensp;{" "}
+                      {editing.cardIndex === index &&
+                      editing.field === "dataType" ? (
+                        <input
+                          type="text"
+                          value={item.dataType}
+                          onChange={(e) => handleChange(e, "dataType", index)}
+                          onBlur={handleBlur}
+                          className="flex text-[14px] bg-transparent h-[21px] w-20 outline-none border-b border-cyan-400 duration-300"
+                        />
+                      ) : (
+                        <p className="underline border-b border-transparent h-[21px]">
+                          {item.dataType}
+                        </p>
+                      )}
+                    </div>
+                    <div
+                      onClick={() => handleClick(index, "traitType")}
+                      className="flex text-[14px]"
+                    >
+                      Data type :&ensp;{" "}
+                      {editing.cardIndex === index &&
+                      editing.field === "traitType" ? (
+                        <input
+                          type="text"
+                          value={item.traitType}
+                          onChange={(e) => handleChange(e, "traitType", index)}
+                          onBlur={handleBlur}
+                          className="flex text-[14px] bg-transparent h-[21px] w-20 outline-none border-b border-cyan-400 duration-300"
+                        />
+                      ) : (
+                        <p className="underline border-b border-transparent h-[21px]">
+                          {item.traitType}
+                        </p>
+                      )}
+                    </div>
+                  </>
+                </div>
+              ))}
+              <div
+                onClick={handleCreateAttribute}
+                className="w-[227px] h-[157px] flex justify-center items-center bg-transparent border border-white rounded-xl p-3 hover:scale-105 cursor-pointer duration-300 "
+              >
+                <img src={Add}></img>
+              </div>
+              <NextPageButton
+              TextTitle="Next"
+              BorderRadius={0}
+              NextPage="/newintregation/8"
+              FontSize={40}
+            ></NextPageButton>
+            </div>
+          </div>
+          <div className="w-2/6 h-5/6 flex flex-col items-end  ">
+            <Conectwalet></Conectwalet>
+            <div className="w-[266px] h-[414px] border-[1px] border-white rounded-xl mt-[30px] flex flex-col items-center py-[10px] z-10">
+              <p className="text-[24px] w-[231px] font-bold text-white text-center">
+                Origin Token Attributes
+              </p>
+              <p className="text-[16px] w-[216px]  text-white pt-[15px]">
+                &emsp;Mostly NFT token will need attributes of it called
+                metadata. Origin attributes is the static type of metadata. And
+                it cannot be change ( but it can be override by token attributes
+                we will discuss next step ).
+              </p>
+              <p className="text-[16px] w-[216px]  text-white pt-[15px]">
+                &emsp; You can edit value of attributes by double click the
+                value and change to your proper design.
+              </p>
+            </div>
+            <div
+              onClick={() => {
+                setIsShow(!isShow);
+              }}
+              className="absolute text-[50px] mt-[750px] ml-[1150px] cursor-pointer hover:scale-150 hover:text-[#262f50] duration-500"
+            >
+              ?
+            </div>
+          </div>
         </div>
-    )
-}
+        {isShow ? (
+          <div
+            className="absolute duration-500"
+            onClick={() => {
+              setIsShow(!isShow);
+            }}
+          >
+            <Darkbg></Darkbg>
+            <div className="absolute mt-[140px] ml-[-350px] z-20">
+              <img
+                src={logo3}
+                className="mt-[80px] mr-[150px] rotate-[160deg] "
+              ></img>
+              <p className="ml-[300px] mt-[-60px] absolute w-full font-bold">
+                Double Click to edit
+              </p>
+            </div>
+            <div className="absolute mt-[0px] ml-[-430px] z-20">
+              <img
+                src={logo2}
+                className="mt-[50px] mr-[200px] rotate-[160deg] "
+              ></img>
+              <p className="ml-[-190px] mt-[-185px] absolute w-full font-bold">
+                Click to delete component
+              </p>
+            </div>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default NewIntregation7
+export default NewIntregation7;
--- src/component/Connectwallet.tsx
+++ src/component/Connectwallet.tsx
@@ -1,147 +1,195 @@
-import { useEffect, useState } from 'react'
-import logo1 from '../pic/Keplr_Icon 1.png'
-import logo2 from '../pic/Copy.png'
-import logo3 from '../pic/Share.png'
-import logo4 from '../pic/SIX_Token_Icon 1.png'
-import logo5 from '../pic/X.png'
+import { useEffect, useState } from "react";
+import logo1 from "../pic/Keplr_Icon 1.png";
+import logo2 from "../pic/Copy.png";
+import logo3 from "../pic/Share.png";
+import logo4 from "../pic/SIX_Token_Icon 1.png";
+import logo5 from "../pic/X.png";
 import { StargateClient } from "@cosmjs/stargate";
 //Redux
 import { useSelector } from "react-redux";
 import {
-    walletcounterSelector,
-    setisloggin,
-    setAddress,
-    setBalance
+  walletcounterSelector,
+  setisloggin,
+  setAddress,
+  setBalance,
 } from "../store/slices/walletcounterSlice";
 import { useAppDispatch } from "../store/store";
-import { CircularProgress, LinearProgress, Skeleton } from '@mui/material'
-
-
+import {
+  CircularProgress,
+  LinearProgress,
+  Skeleton,
+  Tooltip,
+} from "@mui/material";
 
 const Conectwalet = () => {
-    //Redux
-    const dispatch = useAppDispatch();
-    const walletcounterReducer = useSelector(walletcounterSelector);
-    // Usetstate for storing wallets details state.
-    const [web3, setWeb3] = useState(null);
-    // Loading
-    const [isLoading, setisLoading] = useState(false);
-    // chain detail
-    const [chainId, setChainId] = useState("fivenet")
-    const [token, setToken] = useState("usix")
-    const [rpcEndpoint, setRpcEndpoint] = useState("https://rpc1.fivenet.sixprotocol.net/")
-    const [exponent, setExponent] = useState(1e6)
-    const [tokenName, setTokenName] = useState("SIX")
+  //Redux
+  const dispatch = useAppDispatch();
+  const walletcounterReducer = useSelector(walletcounterSelector);
+  // Usetstate for storing wallets details state.
+  // Loading
+  const [isLoading, setisLoading] = useState(false);
+  // chain detail
+  const [chainId, setChainId] = useState("fivenet");
+  const [token, setToken] = useState("usix");
+  const [rpcEndpoint, setRpcEndpoint] = useState(
+    "https://rpc1.fivenet.sixprotocol.net/"
+  );
+  const [exponent, setExponent] = useState(1e6);
 
-    const buttonLogout = async () => {
-        dispatch(setisloggin(false));
-        dispatch(setAddress(""));
-        dispatch(setBalance(0));
+  const [cosmosAddress, setCosmosAddress] = useState("");
+
+  const [copied, setCopied] = useState(false);
+
+  const buttonLogout = async () => {
+    dispatch(setisloggin(false));
+    dispatch(setAddress(""));
+    dispatch(setBalance(0));
+  };
+
+  // Connect Keplr
+  const buttonHandlerKeplrConnect = async () => {
+    if (window.keplr) {
+      setisLoading(true);
+      // Unlock the wallet.
+      await window.keplr.enable(chainId);
+      // Use offlineSigner to get first wallet and public key.
+      // Currently only first address is supported.
+      const offlineSigner = await window.getOfflineSigner(chainId);
+      const keplrAccounts = await offlineSigner.getAccounts();
+      // Set state value as first address.
+      console.log("KEPL ADREESS :" + keplrAccounts[0].address);
+      dispatch(setAddress(keplrAccounts[0].address));
+      setCosmosAddress(keplrAccounts[0].address);
+    } else {
+      alert("Keplr extension is not installed.");
     }
+  };
 
-    // Connect Keplr
-    const buttonHandlerKeplrConnect = async () => {
-        if (window.keplr) {
-            setisLoading(true)
-            // Unlock the wallet.
-            await window.keplr.enable(chainId);
-            // Use offlineSigner to get first wallet and public key.
-            // Currently only first address is supported.
-            const offlineSigner = await window.getOfflineSigner(chainId);
-            const keplrAccounts = await offlineSigner.getAccounts();
-            // Set state value as first address.
-            console.log("KEPL ADREESS :" + keplrAccounts[0].address)
+  //Get balance
+  const getKeplrBalance = async () => {
+    // Use StargateClient and RPC because of its lightweight payloads and high performance.
+    const client = await StargateClient.connect(rpcEndpoint);
 
-            dispatch(setAddress(keplrAccounts[0].address));
-        } else {
-            alert("Keplr extension is not installed.");
-        }
-    };
+    // Get balance as Coin.
+    // Amount is the number of coins, while denom is the identifier of the coins.
+    const balanceAsCoin = await client.getBalance(
+      walletcounterReducer.cosmosaddress,
+      token
+    );
+    const balance = (parseInt(balanceAsCoin.amount) * 1) / exponent;
 
-    //Get balance
-    const getKeplrBalance = async () => {
+    // Set state values.
+    dispatch(setBalance(balance.toFixed(2)));
+    dispatch(setisloggin(true));
+    setisLoading(false);
+  };
 
+  const getSignature = async () => {
+    if (cosmosAddress) {
+      try {
+        const offlineSigner = window.getOfflineSigner(chainId);
+        const signedMessage = await offlineSigner.keplr.signArbitrary(
+          chainId,
+          cosmosAddress,
+          "My Message"
+        );
+        console.log(signedMessage);
+        console.log(signedMessage.pub_key);
+        const verified = await offlineSigner.keplr.verifyArbitrary(
+          chainId,
+          cosmosAddress,
+          "My Message",
+          signedMessage
+        );
+        console.log("verified= ", verified);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+  };
 
-        // Use StargateClient and RPC because of its lightweight payloads and high performance.
-        const client = await StargateClient.connect(rpcEndpoint);
+  const copyToClipboard = (value) => {
+    navigator.clipboard.writeText(value);
+    setCopied(true);
+  };
 
-        // Get balance as Coin.
-        // Amount is the number of coins, while denom is the identifier of the coins.
-        const balanceAsCoin = await client.getBalance(walletcounterReducer.cosmosaddress, token);
-        const balance = parseInt(balanceAsCoin.amount) * 1 / exponent;
+  useEffect(() => {
+    {
+      walletcounterReducer.isloggin && getKeplrBalance();
+    }
+    getSignature();
+  }, [cosmosAddress]);
 
-        // Set state values.        
-        dispatch(setBalance(balance.toFixed(2)));
-        dispatch(setisloggin(true));
-        setisLoading(false)
+  useEffect(() => {
+    getKeplrBalance();
+  }, [walletcounterReducer.cosmosaddress]);
 
-    };
-
-    const initializeWeb3 = async () => {
-        try {
-            if (window.ethereum) {
-                // Modern dapp browsers (like MetaMask)
-                const web3Instance = new Web3(window.ethereum);
-                setWeb3(web3Instance);
-                // await connectToWallet();
-            } else {
-                console.error("No wallet detected.");
-            }
-        } catch (error) {
-            console.error("Error initializing Web3:", error);
-        }
-    };
-
-    // Get data for set web3
-    useEffect(() => {
-        initializeWeb3();
-        // getData();
-
-    }, []);
-
-    useEffect(() => {
-       getKeplrBalance()
-
-
-    }, [walletcounterReducer.cosmosaddress]);
-
-    return (
-        <div className='w-[266px] h-[95px] border-[1px] border-white rounded-xl px-[12px] py-[9px]'>
-            {isLoading ?
-                <CircularProgress className='mt-[15px] ml-[100px]'></CircularProgress>
-                :
-                <div>
-                    {walletcounterReducer.isloggin ?
-                        <div>
-                            <div className=' flex justify-between'>
-                                <p className=' text-[20px] ml-2  text-gray-200'>{walletcounterReducer.cosmosaddress.substring(0, 8)}...{walletcounterReducer.cosmosaddress.substring(walletcounterReducer.cosmosaddress.length - 4, walletcounterReducer.cosmosaddress.length)}</p>
-                                <div className='flex justify-between items-center w-[65px]'>
-                                    <img src={logo1} className='w-[17px] h-[17px]'></img>
-                                    <img src={logo2} className='w-[18px] h-[16px]'></img>
-                                    <img src={logo3} className='w-[18px] h-[18px] mt-2'></img>
-                                </div>
-                            </div>
-                            <div className='mt-3 flex '>
-                                <div className='flex w-[50%]  items-center'>
-                                    <img src={logo4} className='w-[31px] h-[31px]'></img>
-                                    <p className='text-gray-200 text-[24px] mx-8'>{walletcounterReducer.cosmosbalance}</p>
-
-                                </div>
-                                <div className='flex w-[50%] justify-end items-center mx-13'>
-                                    <p className='text-[#D9D9D9] text-[24px] border-[0.5px] border-white rounded-[100%] px-3 mr-2 '>5</p>
-                                    <img src={logo5} onClick={buttonLogout} className='border-[0.5px] rounded-xl p-2 hover:scale-105 duration-500 cursor-pointer'></img>
-                                </div>
-                            </div>
-                        </div>
-                        :
-                        <div className='flex justify-center items-center h-[80px]'>
-                            <p className='text-3xl text-white cursor-pointer hover:scale-105 duration-500' onClick={buttonHandlerKeplrConnect}>Connect Wallet</p>
-                        </div>
-                    }
+  return (
+    <div className="w-[266px] h-[95px] border-[1px] border-white rounded-xl px-[12px] py-[9px]">
+      {isLoading ? (
+        <CircularProgress className="mt-[15px] ml-[100px]"></CircularProgress>
+      ) : (
+        <div>
+          {walletcounterReducer.isloggin ? (
+            <div>
+              <div className=" flex justify-between">
+                <p className=" text-[20px] ml-2  text-gray-200">
+                  {walletcounterReducer.cosmosaddress.substring(0, 8)}...
+                  {walletcounterReducer.cosmosaddress.substring(
+                    walletcounterReducer.cosmosaddress.length - 4,
+                    walletcounterReducer.cosmosaddress.length
+                  )}
+                </p>
+                <div className="flex justify-between items-center w-[65px]">
+                  <img src={logo1} className="w-[17px] h-[17px]"></img>
+                  <Tooltip
+                    title={copied ? "Copied" : "Copy to Clipboard"}
+                    arrow
+                  >
+                    <img
+                      src={logo2}
+                      className="w-[18px] h-[16px] cursor-pointer"
+                      onClick={() =>
+                        copyToClipboard(walletcounterReducer.cosmosaddress)
+                      }
+                    ></img>
+                  </Tooltip>
+                  <img src={logo3} className="w-[18px] h-[18px] mt-2"></img>
                 </div>
-            }
+              </div>
+              <div className="mt-3 flex ">
+                <div className="flex w-[50%]  items-center">
+                  <img src={logo4} className="w-[31px] h-[31px]"></img>
+                  <p className="text-gray-200 text-[24px] mx-8">
+                    {walletcounterReducer.cosmosbalance}
+                  </p>
+                </div>
+                <div className="flex w-[50%] justify-end items-center mx-13">
+                  <p className="text-[#D9D9D9] text-[24px] border-[0.5px] border-white rounded-[100%] px-3 mr-2 ">
+                    5
+                  </p>
+                  <img
+                    src={logo5}
+                    onClick={buttonLogout}
+                    className="border-[0.5px] rounded-xl p-2 hover:scale-105 duration-500 cursor-pointer"
+                  ></img>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="flex justify-center items-center h-[80px]">
+              <p
+                className="text-3xl text-white cursor-pointer hover:scale-105 duration-500"
+                onClick={buttonHandlerKeplrConnect}
+              >
+                Connect Wallet
+              </p>
+            </div>
+          )}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default Conectwalet
+export default Conectwalet;
